a2821dd525bc164881436a8cff3689af0d88c08f
Sandeep Nunna <snunna@centrak.com>
2023-01-01 21:53:25 -0500

    Definte interface for the product slice of state

--- APM/src/app/products/state/product.reducer.ts
+++ APM/src/app/products/state/product.reducer.ts
@@ -1,4 +1,11 @@
 import { createAction, createReducer, on } from "@ngrx/store";
+import { Product } from "../product";
+
+export interface ProductState {
+  showProductCode: boolean;
+  currentProduct: Product;
+  products: Product[];
+}
 
 export const productReducer = createReducer(
   { showProductCode: true },
