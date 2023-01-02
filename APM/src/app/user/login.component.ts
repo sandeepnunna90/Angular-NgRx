63c2d97808dd114fa63107543469465b50b993ee
Sandeep Nunna <snunna@centrak.com>
2023-01-01 23:02:08 -0500

    Homework 2: - Strongly type UserState in user reducer - Built selectors for maskUserName & currentUser - Modify the reducer to use the strongly typed state - Modify the login component to use the strongly typed state and selector

--- APM/src/app/user/login.component.ts
+++ APM/src/app/user/login.component.ts
@@ -2,8 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
+import { State } from '../state/app.state';
 
 import { AuthService } from './auth.service';
+import { getMaskUserName } from './state/user.reducer';
 
 @Component({
   templateUrl: './login.component.html',
@@ -14,18 +16,13 @@ export class LoginComponent implements OnInit {
 
   maskUserName: boolean;
 
-  constructor(private store: Store<any>, private authService: AuthService, private router: Router) { }
+  constructor(private store: Store<State>, private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
-
-  this.store.select('user').subscribe(
-    user => {
-      if (user) {
-        this.maskUserName = user.maskUserName;
-      }
-    }
-  )
-
+    // TODO: Unsubscribe
+    this.store.select(getMaskUserName).subscribe(
+      maskUserName => this.maskUserName = maskUserName
+    );
   }
 
   cancel(): void {
--- APM/src/app/user/state/user.reducer.ts
+++ APM/src/app/user/state/user.reducer.ts
@@ -1,8 +1,31 @@
-import { createAction, createReducer, on } from "@ngrx/store";
+import { createAction, createFeatureSelector, createReducer, createSelector, on } from "@ngrx/store";
+import { User } from "../user";
 
-export const userReducer = createReducer(
-  { maskUserName: false },
-  on(createAction('[User] Mask User Name'), state => {
+export interface UserState {
+  maskUserName: boolean;
+  currentUser: User;
+}
+
+const initialState: UserState = {
+  maskUserName: false,
+  currentUser: null
+}
+
+const getUserFeatureState = createFeatureSelector<UserState>('user');
+
+export const getMaskUserName = createSelector(
+  getUserFeatureState,
+  state => state.maskUserName
+);
+
+export const getCurrentUser = createSelector(
+  getUserFeatureState,
+  state => state.currentUser
+);
+
+export const userReducer = createReducer<UserState>(
+  initialState,
+  on(createAction('[User] Mask User Name'), (state): UserState => {
     console.log('original user slice state', state);
     return {
       ...state,
