83b44768b8ee86ccca5836d9f527b58b875553d9
Sandeep Nunna <snunna@centrak.com>
2023-01-18 20:29:32 -0500

    Update the product List Component to use Async Pipe with ProductEffects

--- APM/src/app/products/product-list/product-list.component.ts
+++ APM/src/app/products/product-list/product-list.component.ts
@@ -2,9 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 
 import { Product } from '../product';
-import { ProductService } from '../product.service';
-import { getCurrentProduct, getShowProductCode, State } from '../state/product.reducer';
+import { getCurrentProduct, getProducts, getShowProductCode, State } from '../state/product.reducer';
 import * as ProductActions from '../state/product.actions';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'pm-product-list',
@@ -15,40 +15,21 @@ export class ProductListComponent implements OnInit {
   pageTitle = 'Products';
   errorMessage: string;
 
-  displayCode: boolean;
-
-  products: Product[];
-
   // Used to highlight the selected product in the list
-  selectedProduct: Product | null;
+  products$: Observable<Product[]>;
+  selectedProduct$: Observable<Product>;
+  displayCode$: Observable<boolean>;
 
-  constructor(private store: Store<State>, private productService: ProductService) { }
+  constructor(private store: Store<State>) { }
 
   ngOnInit(): void {
-    // this.sub = this.productService.selectedProductChanges$.subscribe(
-    //   currentProduct => this.selectedProduct = currentProduct
-    // );
+    this.selectedProduct$ = this.store.select(getCurrentProduct);
 
-    // use reducer selector to save current product instead of the above code. 
-    //TODO: Unsubscribe
-    this.store.select(getCurrentProduct).subscribe(
-      currentProduct => this.selectedProduct = currentProduct
-    );
+    this.products$ = this.store.select(getProducts);
 
-    this.productService.getProducts().subscribe({
-      next: (products: Product[]) => this.products = products,
-      error: err => this.errorMessage = err
-    });
+    this.store.dispatch(ProductActions.loadProducts());
 
-    // code without a selector
-    // this.store.select('products').subscribe(
-    //   products => this.displayCode = products.showProductCode
-    // );
-
-    // with selector
-    this.store.select(getShowProductCode).subscribe(
-      (showProductCode) => this.displayCode = showProductCode
-    );
+    this.displayCode$ = this.store.select(getShowProductCode);
   }
 
   checkChanged(): void {
@@ -56,12 +37,10 @@ export class ProductListComponent implements OnInit {
   }
 
   newProduct(): void {
-    // this.productService.changeSelectedProduct(this.productService.newProduct());
     this.store.dispatch(ProductActions.initializeCurrentProduct());
   }
 
   productSelected(product: Product): void {
-    // this.productService.changeSelectedProduct(product);
     this.store.dispatch(ProductActions.setCurrentProduct({ product: product}))
   }
 
