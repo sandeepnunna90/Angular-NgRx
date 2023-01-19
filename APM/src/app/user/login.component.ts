eb536aa7a6c22c31979b2975b9086f27e1e16d5f
Sandeep Nunna <snunna@centrak.com>
2023-01-18 20:50:22 -0500

    Homework 4: Update the maskUserName boolean to use Async Pipe in Login Component

--- APM/src/app/user/login.component.ts
+++ APM/src/app/user/login.component.ts
@@ -7,6 +7,7 @@ import { State } from '../state/app.state';
 import { AuthService } from './auth.service';
 import { getMaskUserName } from './state/user.reducer';
 import * as UserActions from './state/user.actions';
+import { Observable } from 'rxjs';
 
 @Component({
   templateUrl: './login.component.html',
@@ -15,15 +16,12 @@ import * as UserActions from './state/user.actions';
 export class LoginComponent implements OnInit {
   pageTitle = 'Log In';
 
-  maskUserName: boolean;
+  maskUserName$: Observable<Boolean>;
 
   constructor(private store: Store<State>, private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
-    // TODO: Unsubscribe
-    this.store.select(getMaskUserName).subscribe(
-      maskUserName => this.maskUserName = maskUserName
-    );
+    this.maskUserName$ = this.store.select(getMaskUserName);
   }
 
   cancel(): void {
